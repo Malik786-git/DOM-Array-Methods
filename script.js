999265dd88227e4584fcc032e9831524d21956e4
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-01-15 01:27:08 +0500

    project Done
    Alhumduillah!

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const filterBtn = document.getElementById('filter');
 const sortBtn = document.getElementById('sort');
 const sumBtn = document.getElementById('sum');
 
-// initialize data array;
+// initialize data array
 let data = [];
 
 
@@ -17,7 +17,7 @@ async function getRandomUser(params) {
     const res = await fetch('https://randomuser.me/api/');
     //    wait for response to convert into JSON
     const data = await res.json();
-   
+
     // console.log(data);
 
     // Get the user name
@@ -26,8 +26,8 @@ async function getRandomUser(params) {
 
     // Create the New User
     const newUser = {
-      name: `${user.name.title} ${user.name.first} ${user.name.last}`,
-       balance: Math.floor(Math.random()*1000000)  
+        name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+        balance: Math.floor(Math.random() * 1000000)
     }
     // console.log(newUser);
 
@@ -35,29 +35,79 @@ async function getRandomUser(params) {
     addData(newUser);
 }
 
-// Function to add user data into data array
+// Function to add user data into data array...
 
 function addData(newUser) {
 
     data.push(newUser);
     // console.log("dataArray", data);
-    // Update Dom to display users in the data array
+    // Update Dom to display users in the data array...
     updateDOM();
 
 }
+
+// for formated Number as money
+function formatNumberToDollar(number) {
+    return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+}
+
+// function to double money of all users
+function doubleMoney() {
+    data = data.map(user => {
+        return { ...user, balance: user.balance * 2 };
+    })
+    updateDOM();
+}
+
+// FilterMillionaires
+
+function filterMillionaires() {
+    data = data.filter(user => user.balance > 1_000_000);
+    // Update with New User
+    updateDOM();
+}
+
+// sort Wealth 
+function sortWealth() {
+    // sort users by balance using compare function inside sort.
+    data.sort((a,b)=>a.balance - b.balance);
+    updateDOM();
+}
+// for total money of all users
+function totalBalance() {
+    // accumulator start at 0 
+    const balance = data.reduce((acc, user) => acc+=user.balance, 0);
+    console.log(balance);
+    const balanceElement = document.createElement('div');
+    balanceElement.innerHTML = `<h3>Total Balance: ${formatNumberToDollar(balance)}</h3>`;
+    main.appendChild(balanceElement); 
+    
+}
+
 // Update UI
 function updateDOM(userData = data) {
     // clear previous UI
     main.innerHTML = ` <h2><strong>User</strong>Wealth</h2>`;
-    userData.forEach((user)=>{
-           const userDiv= document.createElement('div');
-           userDiv.classList.add('user');
-           userDiv.innerHTML = `<strong>${user.name}</strong>${user.balance}`;
-           main.appendChild(userDiv);
+    userData.forEach((user) => {
+        const userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+
+        userDiv.innerHTML = `<strong>${user.name}</strong>${formatNumberToDollar(user.balance)}`;
+        main.appendChild(userDiv);
     })
 
 }
 
 
-getRandomUser();
-
+// Event listerners...
+// add user
+addUserBtn.onclick = getRandomUser;
+// double money
+doubleBtn.onclick = doubleMoney;
+// filter Millionaires
+filterBtn.onclick = filterMillionaires;
+// sorted wealth
+sortBtn.onclick = sortWealth;
+// sum all balance of user
+sumBtn.onclick = totalBalance;
